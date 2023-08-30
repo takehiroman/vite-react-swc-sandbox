b2579c57854f1916536ee1df866d29050de463c3
津崎　豪宏 <tsuzakitakehiro@LAB-N2819R.local>
2023-08-30 18:38:47 +0900

    prが走るか確認

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const handleClink = () => {
+    setCount((count) => count + 1);
+  };
   return (
     <div className="App">
       <div>
@@ -17,9 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleClink}>count is {count}</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,7 +28,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
